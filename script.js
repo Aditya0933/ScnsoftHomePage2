01f23980cff4e59bf5398b630aecf3b592344648
Aditya Singh Parihar <201037@kit.ac.in>
2023-04-19 20:25:30 +0530

    this is my first commit

---
+++ script.js
@@ -0,0 +1,175 @@
+// Moving Company logo
+
+// window.onload=()=>{
+//     let x = 0;
+//     setInterval(()=>{
+//         x+=190
+//         let left = x + "px"
+//         document.getElementById("main-company-logo").style.left = "-"+left
+//     },2000)  
+    
+// }
+// window.onload=()=>{
+//     document.querySelector(".software-devevopment-content").style.display = "block"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction1(){
+//     document.querySelector(".software-devevopment-content").style.display = "block"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction2(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "block"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction3(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "block"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction4(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "block"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction5(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "block"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction6(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "block"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction7(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "block"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction8(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "block"
+//     document.querySelector(".Cybersecurity-Services").style.display = "none"
+// }
+// function myFunction9(){
+//     document.querySelector(".software-devevopment-content").style.display = "none"
+//     document.querySelector(".Testing-content").style.display = "none"
+//     document.querySelector(".Application-Services").style.display = "none"
+//     document.querySelector(".UX-UI-Design").style.display = "none"
+//     document.querySelector(".IT-Consulting").style.display = "none"
+//     document.querySelector(".Data-Analytics").style.display = "none"
+//     document.querySelector(".Help-Desk-Services").style.display = "none"
+//     document.querySelector(".Infrastructure-Services").style.display = "none"
+//     document.querySelector(".Cybersecurity-Services").style.display = "block"
+// }
+
+
+//  FAQ Section   
+
+// pass by value and pass by reference
+
+
+// const arr = ['lodu','lalit'];
+// const arrb = arr;
+// arrb.push('sonu');
+// console.log(5 == 'a');
+
+
+
+
+// console.log(arrb)
+// console.log(arr);
+
+// parent element
+const parent_sec =  document.querySelector('.explore-heading');
+const containerRight = document.querySelector('.container-right');
+// array of children element
+const children_arr = parent_sec.children;
+
+ 
+for (let i = 0; i < children_arr.length; i++) {
+    children_arr[i].addEventListener('click',(e)=>{
+        removeHeading()
+    e.target.classList.add('over-heading');
+    console.log(e.target.id);
+    const check = document.querySelector('.' +e.target.id + "-content");
+    check.classList.add('software-active')
+   })
+    
+}
+
+function removeHeading(){
+    for (let i = 0; i < children_arr.length; i++) {
+        children_arr[i].classList.remove('over-heading');
+        containerRight.children[i].classList.remove('software-active')
+       }       
+    } 
+
+
+
+
+// FAQ Section
+ const faqChild = document.querySelector(".faq-container")
+ console.log("hiii")
+console.log(faqChild.children[0])
+console.log(faqChild.children)
+console.log(faqChild.children[2])
+console.log(faqChild.children[9])
+console.log(faqChild.children[6])
