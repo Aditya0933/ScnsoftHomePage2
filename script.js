a08389d27a33fea9e3e5d12efa0bee2c6fa252b9
Aditya Singh Parihar <201037@kit.ac.in>
2023-04-22 16:37:40 +0530

    faq

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 //         let left = x + "px"
 //         document.getElementById("main-company-logo").style.left = "-"+left
 //     },2000)  
-    
+
 // }
 // window.onload=()=>{
 //     document.querySelector(".software-devevopment-content").style.display = "block"
@@ -138,38 +138,176 @@
 // console.log(arr);
 
 // parent element
-const parent_sec =  document.querySelector('.explore-heading');
+const parent_sec = document.querySelector('.explore-heading');
 const containerRight = document.querySelector('.container-right');
 // array of children element
 const children_arr = parent_sec.children;
 
- 
+
 for (let i = 0; i < children_arr.length; i++) {
-    children_arr[i].addEventListener('click',(e)=>{
+    children_arr[i].addEventListener('click', (e) => {
         removeHeading()
-    e.target.classList.add('over-heading');
-    console.log(e.target.id);
-    const check = document.querySelector('.' +e.target.id + "-content");
-    check.classList.add('software-active')
-   })
-    
+        e.target.classList.add('over-heading');
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.id);
+        console.log(e.target.id.innerText
+        );
+        console.log("Hii")
+        const check = document.querySelector('.' + e.target.id + "-content");
+        check.classList.add('software-active')
+
+    })
+
 }
 
-function removeHeading(){
+function removeHeading() {
     for (let i = 0; i < children_arr.length; i++) {
         children_arr[i].classList.remove('over-heading');
         containerRight.children[i].classList.remove('software-active')
-       }       
-    } 
-
-
-
+    }
+}
 
 // FAQ Section
- const faqChild = document.querySelector(".faq-container")
- console.log("hiii")
-console.log(faqChild.children[0])
-console.log(faqChild.children)
-console.log(faqChild.children[2])
-console.log(faqChild.children[9])
-console.log(faqChild.children[6])
+// const faqChild = document.querySelector(".faq-container")
+// console.log("hiii")
+// console.log(faqChild.children[0])
+// console.log(faqChild.children)
+// console.log(faqChild.children[2])
+// console.log(faqChild.children[9])
+// console.log(faqChild.children[6])
+
+
+// FAQ Section JS =>
+
+const faqbox = document.getElementsByClassName(".qqq")
+// console.log("HIJIJ")
+
+// console.log(faqbox.children)
+for (let i=0; i<faqbox.length ; i++){
+    faqbox[i].addEventListener('click',function(){
+        this.classList.toggle('active')
+        // console.log(faqbox.children[i].children)
+        // console.log("hii")
+        // console.log(i)
+        // console.log("bye")
+    })
+}   
+const FAQ = document.querySelector('.faq-container')
+console.log('Hiiii')
+console.log(FAQ.children)
+console.log(FAQ.children[0])
+console.log(FAQ.children.length)
+for(let i = 0; i<FAQ.children.length; i++){
+    FAQ.children[i].addEventListener('click',(e)=>{
+        console.log(FAQ.children[i])
+        FAQ.children[i].children[1].classList.toggle('active')
+        FAQ.children[i].children[0].children[1].classList.toggle('rotate-arrow-icon')
+    })
+    // console.log(FAQ.children[i])
+    // console.log("Yes")
+}
+console.log('Bye')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function HideParaFun1(){
+//     const details = document.querySelector(".para-block-1");
+//     details.classList.toggle("qaz")
+// }
+// function HideParaFun1() {
+//     const details = document.querySelector(".para-block-1");
+//     if (details.style.display === "none") {
+//         details.style.display = "block";
+//     } else {
+//         details.style.display = "none";
+//     }
+//   }
+// function HideParaFun2() {
+//     const details = document.querySelector(".para-block-2");
+//     if (details.style.display === "none") {
+//         details.style.display = "block";
+//     } else {
+//         details.style.display = "none";
+//     }
+//   }
+// function HideParaFun3() {
+//     const details = document.querySelector(".para-block-3");
+//     if (details.style.display === "none") {
+//         details.style.display = "block";
+//     } else {
+//         details.style.display = "none";
+//     }
+//   }
+// function HideParaFun4() {
+//     const details = document.querySelector(".para-block-4");
+//     if (details.style.display === "none") {
+//         details.style.display = "block";
+//     } else {
+//         details.style.display = "none";
+//     }
+//   }
+// function HideParaFun5() {
+//     const details = document.querySelector(".para-block-5");
+//     if (details.style.display === "none") {
+//         details.style.display = "block";
+//     } else {
+//         details.style.display = "none";
+//     }
+//   }
+// function HideParaFun6() {
+//     const details = document.querySelector(".para-block-6");
+//     if (details.style.display === "none") {
+//         details.style.display = "block";
+//     } else {
+//         details.style.display = "none";
+//     }
+
+//     // for rotating the Icon
+
+//     const rotate = document.querySelector(".rotatearr");
+//     rotate.style.classList.toggle("rotate-arrow")
+//     // rotate.style.console.log("WERTYHUJK")
+//   }
+
+
+// const faq = document.querySelector(".faq-container");
+// console.log(faq.children)
+// const FaqChild = faq.children
+// console.log(FaqChild[0].children[1].className)
+// console.log(faq.children[0].children)
+
+// const faqc1 = document.querySelector(".faq-1");
+// console.log(faqc1.children)
+
+// for (let i = 0; i < FaqChild.length; i++) {
+//     FaqChild[i].addEventListener('click', (event) => {
+//         console.log(i)
+//         console.log(event)
+//         console.log(event.target);
+//         console.log(event.target.id);
+//         console.log(event.target.className);
+//         console.log(event.target.id.innerText);
+//         console.log("Hii")
+//     })
+// }
+
+// console.log(faq[faq-1].children)
+
+
