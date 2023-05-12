afa0487cb2418ae8d30987c59f3fffe99dea3da4
Aditya Singh Parihar <201037@kit.ac.in>
2023-05-12 17:15:19 +0530

    Fixing Extra Space in JS

--- script.js
+++ script.js
@@ -2,163 +2,12 @@
 var copy = document.querySelector(".main-company-logo").cloneNode(true)
 var hey = document.querySelector('.main-div-img-container').appendChild(copy)
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Moving Company logo
-
-// window.onload=()=>{
-//     let x = 0;
-//     setInterval(()=>{
-//         x+=190
-//         let left = x + "px"
-//         document.getElementById("main-company-logo").style.left = "-"+left
-//     },2000)  
-
-// }
-// window.onload=()=>{
-//     document.querySelector(".software-devevopment-content").style.display = "block"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction1(){
-//     document.querySelector(".software-devevopment-content").style.display = "block"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction2(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "block"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction3(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "block"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction4(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "block"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction5(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "block"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction6(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "block"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction7(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "block"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction8(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "block"
-//     document.querySelector(".Cybersecurity-Services").style.display = "none"
-// }
-// function myFunction9(){
-//     document.querySelector(".software-devevopment-content").style.display = "none"
-//     document.querySelector(".Testing-content").style.display = "none"
-//     document.querySelector(".Application-Services").style.display = "none"
-//     document.querySelector(".UX-UI-Design").style.display = "none"
-//     document.querySelector(".IT-Consulting").style.display = "none"
-//     document.querySelector(".Data-Analytics").style.display = "none"
-//     document.querySelector(".Help-Desk-Services").style.display = "none"
-//     document.querySelector(".Infrastructure-Services").style.display = "none"
-//     document.querySelector(".Cybersecurity-Services").style.display = "block"
-// }
-
-
-//  FAQ Section   
-
-// pass by value and pass by reference
-
-
-// const arr = ['lodu','lalit'];
-// const arrb = arr;
-// arrb.push('sonu');
-// console.log(5 == 'a');
-
-
-
-
-// console.log(arrb)
-// console.log(arr);
-
 // parent element
 const parent_sec = document.querySelector('.explore-heading');
 const containerRight = document.querySelector('.container-right');
+
 // array of children element
 const children_arr = parent_sec.children;
-
 
 for (let i = 0; i < children_arr.length; i++) {
     children_arr[i].addEventListener('click', (e) => {
@@ -167,14 +16,11 @@ for (let i = 0; i < children_arr.length; i++) {
         console.log(e);
         console.log(e.target);
         console.log(e.target.id);
-        console.log(e.target.id.innerText
-        );
+        console.log(e.target.id.innerText);
         console.log("Hii")
         const check = document.querySelector('.' + e.target.id + "-content");
         check.classList.add('software-active')
-
     })
-
 }
 
 function removeHeading() {
@@ -184,12 +30,7 @@ function removeHeading() {
     }
 }
 
-
-
-
 // Tech trends Section
-
-
 const TechIcon = document.querySelector(".trends-icon")
 const TechbBottom = document.querySelector(".trends-bottom")
 const TechIconChild = TechIcon.children
@@ -201,65 +42,15 @@ for (let i = 0; i<TechIconChild.length;i++){
     TechbBottomChild[i].classList.add('disblock-1')
     })
 }
+
 function TechBottomBlock(){
     for (let i=0; i<TechbBottomChild.length; i++){
-        TechbBottomChild[i].classList.
-        remove('disblock-1') 
+        TechbBottomChild[i].classList.remove('disblock-1') 
         TechbBottomChild[0].classList.add('disnone-1')
     }
 }
 
-// console.log(TechIcon.children)
-// console.log(TechIcon.children[0])
-// console.log(TechIcon.children[0].children[0])
-// TechIcon.children[0].addEventListener('click',(e)=>{
-//     TechIcon.children[0].children[0].classList.toggle('Yeah_I_am_Toggle_Class') 
-//     console.log('What r u Doing...')
-// })
-// console.log(TechbBottom.children)
-// console.log(TechbBottom.children[0])
-// console.log(TechbBottom.children[0].children[0])
-
-// TechbBottom.children[0].addEventListener('click',(e)=>{
-//     TechIcon.children[0].children[0].classList.toggle('Yeah_I_am_Toggle_Class') 
-//     console.log('Hey What Happend')
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// FAQ Section
-// const faqChild = document.querySelector(".faq-container")
-// console.log("hiii")
-// console.log(faqChild.children[0])
-// console.log(faqChild.children)
-// console.log(faqChild.children[2])
-// console.log(faqChild.children[9])
-// console.log(faqChild.children[6])
-
-
-
-
-
-
 // Technology and Plateform Section 
-
 const plateform = document.querySelector('.plateform-conatiner')
 const technology = document.querySelector('.technology-container')
 const plateformChild = plateform.children
@@ -271,6 +62,7 @@ for( let i=0; i<plateformChild.length; i++){
         technologyChild[i].style.display = "block"
     })
 }
+
 function technologyContainerDisplayNone(){
     for (let i=0;i<technologyChild.length;i++){
         technologyChild[i].style.display = "none"
@@ -278,15 +70,12 @@ function technologyContainerDisplayNone(){
 }
 
 // Custumers Says Section
-
 const cominfo = document.querySelector('.custumers-main-left')
 const comimg = document.querySelector('.custumers-main-right')
 const cominfoChild = cominfo.children
 const comimgChild = comimg.children
-console.log(comimgChild.length)
-console.log('yusssssss')
+
 for (let i=0; i<comimgChild.length-1;i++){
-    
     comimgChild[i].addEventListener('click',()=>{
         custumersmainleft()
         cominfoChild[i].style.display = 'block'
@@ -295,6 +84,7 @@ for (let i=0; i<comimgChild.length-1;i++){
         comimgChild[i].children[0].style.opacity = 'none'
     })
 }
+
 function custumersmainleft(){
     for (let i=0; i<cominfoChild.length; i++){
         cominfoChild[i].style.display = 'none'
@@ -304,181 +94,25 @@ function custumersmainleft(){
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // FAQ Section JS =>
-
 const faqbox = document.getElementsByClassName(".qqq")
-// console.log("HIJIJ")
+const FAQ = document.querySelector('.faq-container')
 
 // console.log(faqbox.children)
 for (let i=0; i<faqbox.length ; i++){
     faqbox[i].addEventListener('click',function(){
         this.classList.toggle('active')
-        // console.log(faqbox.children[i].children)
-        // console.log("hii")
-        // console.log(i)
-        // console.log("bye")
     })
 }   
-const FAQ = document.querySelector('.faq-container')
-// console.log('Hiiii')
-// console.log(FAQ.children)
-// console.log(FAQ.children[0])
-// console.log(FAQ.children.length)
+
 for(let i = 0; i<FAQ.children.length; i++){
     FAQ.children[i].addEventListener('click',(e)=>{
         console.log(FAQ.children[i])
         FAQ.children[i].children[1].classList.toggle('active')
         FAQ.children[i].children[0].children[1].classList.toggle('rotate-arrow-icon')
     })
-    // console.log(FAQ.children[i])
-    // console.log("Yes")
 }
-// console.log('Bye')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function HideParaFun1(){
-//     const details = document.querySelector(".para-block-1");
-//     details.classList.toggle("qaz")
-// }
-// function HideParaFun1() {
-//     const details = document.querySelector(".para-block-1");
-//     if (details.style.display === "none") {
-//         details.style.display = "block";
-//     } else {
-//         details.style.display = "none";
-//     }
-//   }
-// function HideParaFun2() {
-//     const details = document.querySelector(".para-block-2");
-//     if (details.style.display === "none") {
-//         details.style.display = "block";
-//     } else {
-//         details.style.display = "none";
-//     }
-//   }
-// function HideParaFun3() {
-//     const details = document.querySelector(".para-block-3");
-//     if (details.style.display === "none") {
-//         details.style.display = "block";
-//     } else {
-//         details.style.display = "none";
-//     }
-//   }
-// function HideParaFun4() {
-//     const details = document.querySelector(".para-block-4");
-//     if (details.style.display === "none") {
-//         details.style.display = "block";
-//     } else {
-//         details.style.display = "none";
-//     }
-//   }
-// function HideParaFun5() {
-//     const details = document.querySelector(".para-block-5");
-//     if (details.style.display === "none") {
-//         details.style.display = "block";
-//     } else {
-//         details.style.display = "none";
-//     }
-//   }
-// function HideParaFun6() {
-//     const details = document.querySelector(".para-block-6");
-//     if (details.style.display === "none") {
-//         details.style.display = "block";
-//     } else {
-//         details.style.display = "none";
-//     }
-
-//     // for rotating the Icon
-
-//     const rotate = document.querySelector(".rotatearr");
-//     rotate.style.classList.toggle("rotate-arrow")
-//     // rotate.style.console.log("WERTYHUJK")
-//   }
-
-
-// const faq = document.querySelector(".faq-container");
-// console.log(faq.children)
-// const FaqChild = faq.children
-// console.log(FaqChild[0].children[1].className)
-// console.log(faq.children[0].children)
-
-// const faqc1 = document.querySelector(".faq-1");
-// console.log(faqc1.children)
-
-// for (let i = 0; i < FaqChild.length; i++) {
-//     FaqChild[i].addEventListener('click', (event) => {
-//         console.log(i)
-//         console.log(event)
-//         console.log(event.target);
-//         console.log(event.target.id);
-//         console.log(event.target.className);
-//         console.log(event.target.id.innerText);
-//         console.log("Hii")
-//     })
-// }
-
-// console.log(faq[faq-1].children)
-
-
-
-
-
-
-
-
-
 
 // partner-company Section
-
 var cpoy = document.querySelector('.company').cloneNode(true)
 document.querySelector('.partner-company').appendChild(cpoy)
-// const mainimgdiv = document.querySelector(".partner-company")
-// console.log(mainimgdiv)
-// console.log(mainimgdiv.children.length)
-
-// var myVar = setInterval(movingContainer,2000)
-
-// function movingContainer(){ 
-//     // console.log(mainimgdiv.children[0])
-//     // console.log(mainimgdiv.children[1])
-//     // mainimgdiv.removeChild(mainimgdiv.children[0])
-//     // mainimgdiv.removeChild(mainimgdiv.children[1])
-//     mainimgdiv.appendChild(mainimgdiv.children[0])
-//     console.log(mainimgdiv.children[0])
-//     // mainimgdiv.appendChild(mainimgdiv.children[1])
-//     // console.log(mainimgdiv.children[1])
-//     console.log(mainimgdiv.children.length)
-// }
